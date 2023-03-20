af4bd442cadbacaf06935e198af15628aee570f1
Gabriel <caampos22@gmail.com>
2023-03-20 05:48:19 -0300

    :bug: fix: fix add SEO tag to login page

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Navbar, Footer, Form } from '@/containers'
+import { SEO } from '@/components'
 
 
 const SigninPage = () => {
 
   return (
     <>
+      <SEO
+        title='Welcome to the future'
+        description='Search smarter, not harder.'
+        image='assets/twitter_img.png'
+      />
       <Navbar />
       <Form />
       <Footer />
