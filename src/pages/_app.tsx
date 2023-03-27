02ed4abd6ee728742df1e7ede45782a621b52df6
Gabriel <caampos22@gmail.com>
2023-03-27 19:31:47 -0300

    :bug: fix: remove tailwind declarations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import localFont from '@next/font/local'
 import type { AppProps } from 'next/app'
 
 import GlobalStyle from '../styles/globalStyles'
-import 'tailwindcss/tailwind.css'
 
 export const poppins = localFont({
   src: '../../public/fonts/Poppins-Regular.ttf',
