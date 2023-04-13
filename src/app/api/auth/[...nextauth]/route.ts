b0123b253314bdbeab00bac9b8cdb4392ddab414
Gabriel <caampos22@gmail.com>
2023-04-13 14:41:13 -0300

    :bug: fix: allowing google to refresh tokens

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -12,6 +12,13 @@ export const authOptions: NextAuthOptions = {
 		GoogleProvider({
 			clientId: String(process.env.GOOGLE_ID),
 			clientSecret: String(process.env.GOOGLE_SECRET),
+			authorization: {
+				params: {
+					prompt: "consent",
+					access_type: "offline",
+					response_type: "code"
+				}
+			}
 		}),
 		// CredentialsProvider({
 		// 	name: "Sign in",
