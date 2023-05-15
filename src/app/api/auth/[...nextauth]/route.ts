c2fae8d998c63c6f391b0eebff21577ca2f9fad7
Gabriel <caampos22@gmail.com>
2023-05-15 01:45:00 -0300

    🐞 fix: fix redirect_uri

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -17,7 +17,7 @@ export const authOptions: NextAuthOptions = {
 					prompt: "consent",
 					access_type: "offline",
 					response_type: "code",
-					redirect_uri: String(process.env.NEXTAUTH_URL) + "/api/auth/callback/google",
+					redirect_uri: String(process.env.NEXTAUTH_URL),
 				},
 			},
 		}),
